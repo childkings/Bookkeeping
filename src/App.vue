accd47e2b206b2f965db5cde62d89f9e52d65fca
peakTime <2441636874@qq.com>
2022-02-17 19:29:42 +0800

    记账app实现

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,86 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="container">
+    <router-view :labelList='labelList' @getRecordData="dataRecord" @getDelData="dataDel" @getAddData="addData" :record="record"></router-view>
+    <div class="switch">
+      <router-link to="/operation"><van-icon name="records" size="25"/><span>记账</span></router-link>
+      <router-link to="/label"><van-icon name="description" size="25"/><span>标签</span></router-link>
+      <router-link to="/count"><van-icon name="balance-list-o" size="25"/><span>统计</span></router-link>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data () {
+    return {
+      labelList: [
+        { id: 1, name: '衣服', el: 'el-icon-s-custom', state: false },
+        { id: 2, name: '吃饭', el: 'el-icon-potato-strips', state: false },
+        { id: 3, name: '零食', el: 'el-icon-lollipop', state: false },
+        { id: 4, name: '水果', el: 'el-icon-grape', state: false }
+      ],
+      record: [
+        { id: 1, name: '零食', state: false, remarks: '超市买零食', money: 234 },
+        { id: 2, name: '衣服', state: false, remarks: '买了两件新年装', money: 586 }
+      ]
     }
+  },
+  methods: {
+    dataRecord (val) {
+      val.id = this.record.length + 1
+      this.record.push(val)
+      this.labelList.forEach(val => { val.state = false })
+    },
+    dataDel (val) {
+      this.labelList = this.labelList.filter(item => item.id !== val)
+    },
+    addData (val) {
+      val.id = this.labelList.length + 1
+      val.state = false
+      this.labelList.push(val)
+    }
+  },
+  updated () {
+    document.querySelectorAll('a').forEach(val => {
+      if (val.classList.contains('router-link-exact-active')) {
+        val.style.color = 'rgb(25, 137, 250)'
+      } else {
+        val.style.color = 'rgb(77,77,77)'
+      }
+    })
+  }
+}
+</script>
+
+<style lang="less">
+* {
+  box-sizing: border-box;
+}
+.switch {
+  display: flex;
+  justify-content: space-between;
+  position: fixed ;
+  bottom: 0;
+  width: 100vw;
+  height: 9%;
+  border-top: 1px solid rgb(25, 137, 250);
+  >a {
+    display: flex;
+    flex-direction: column;
+    justify-content:center;
+    align-items: center;
+    flex: 1;
+    background-color: #fff;
+    font-size: 10px;
+    color: rgb(77, 77, 77);
+    i {
+      margin-bottom: 5px;
+    }
+  }
+  .router-link-exact-active,.router-link-active {
+    border:solid rgb(25, 137, 250);
+    border-width: 4px 0;
+    padding-bottom: 5px;
   }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,37 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Operation from '@/views/Operation.vue'
+import Label from '@/views/Label.vue'
+import Count from '@/views/Count.vue'
+import LabelDisplay from '@/components/LabelDisplay.vue'
+import LabelAddIcon from '@/components/LabelAddIcon.vue'
+import LabelAddMessage from '@/components/LabelAddMessage.vue'
+import DataDisplay from '@/components/DataDisplay.vue'
+import ViewDisplay from '@/components/ViewDisplay.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/', redirect: '/operation' },
+  { path: '/operation', component: Operation },
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/label',
+    component: Label,
+    redirect: '/label/labelDisplay',
+    children: [
+      { path: 'labelDisplay', component: LabelDisplay },
+      { path: 'labelAddIcon', component: LabelAddIcon },
+      { path: 'labelAddMessage', component: LabelAddMessage }
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/count',
+    component: Count,
+    redirect: '/count/dataDisplay',
+    children: [
+      { path: 'dataDisplay', component: DataDisplay },
+      { path: 'viewDisplay', component: ViewDisplay }
+    ]
   }
 ]
 
@@ -25,5 +40,4 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
-
 export default router
